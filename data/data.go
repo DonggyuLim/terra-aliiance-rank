76ef8c649c0bb873313c973cd8914e371d9f85af
gyu <udt6236@gamil.com>
2023-02-28 18:45:43 +0900

    chain start

--- data/data.go
+++ data/data.go
@@ -38,11 +38,11 @@ const (
 func Main(wg *sync.WaitGroup) {
 	defer wg.Done()
 	w := &sync.WaitGroup{}
-	w.Add(1)
-	// go MakeReward(w, ATREIDES)
+	w.Add(5)
+	go MakeReward(w, ATREIDES)
 	go MakeReward(w, Harkonnen)
-	// go MakeReward(w, CORRINO)
-	// go MakeReward(w, ORDOS)
+	go MakeReward(w, CORRINO)
+	go MakeReward(w, ORDOS)
 	go MakeTotal(w)
 	wg.Wait()
 }
