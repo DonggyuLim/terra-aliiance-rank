c2c73ae038d87b6a78d6f2ea30170307a2af8e53
gyu <udt6236@gamil.com>
2023-02-28 19:07:37 +0900

    harkonnen

--- main.go
+++ main.go
@@ -5,18 +5,17 @@ import (
 
 	"github.com/DonggyuLim/Alliance-Rank/data"
 	"github.com/DonggyuLim/Alliance-Rank/db"
-	"github.com/DonggyuLim/Alliance-Rank/rest"
 )
 
 func main() {
 	wg := &sync.WaitGroup{}
-	wg.Add(2)
+	wg.Add(1)
 	db.Connect()
 	defer db.Close()
 
 	go data.Main(wg)
 
-	go rest.Start(wg)
+	// go rest.Start(wg)
 
 	wg.Wait()
 }
